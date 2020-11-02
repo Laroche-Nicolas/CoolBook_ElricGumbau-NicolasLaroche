9dee9b21cd11fcf09d3b3bcac8748da5f514694e
Nicolas Laroche <nicolas.laroche@ynov.com>
2020-11-02 18:37:08 +0100

    debut count by genre

--- src/components/CoolBookGenres.vue
+++ src/components/CoolBookGenres.vue
@@ -6,7 +6,7 @@
   <ul v-for="genre in allGenres" :key="genre.id" class="list-group">
   <li class="list-group-item d-flex justify-content-between align-items-center">
     {{genre.label}}
-    <span class="badge badge-primary badge-pill">14</span>
+    <span class="badge badge-primary badge-pill">{{countByGenre(genre.id)}}</span>
   </li>
 </ul>
 
@@ -17,23 +17,45 @@
 <script>
 import axios from "axios";
 
-const API_BOOKS_ENDPOINT = '  http://localhost:3000/genres/'
+const API_GENRES_ENDPOINT = '  http://localhost:3000/genres/'
+const API_BOOKS_ENDPOINT = '  http://localhost:3000/books/'
 export default {
 name: 'CoolBookGenre',
   props: {
   },
   data:()=>({
-    allGenres:[]
+    allGenres:[],
+    allBooks:[],
   }),
   methods:{
     async fetchAllGenres(){
-      let genres = await axios.get(`${API_BOOKS_ENDPOINT}`);
+      let genres = await axios.get(`${API_GENRES_ENDPOINT}`);
       this.allGenres = genres.data
       console.log(this.allGenres)
     },
+    async fetchAllBooks(){
+      let books = await axios.get(`${API_BOOKS_ENDPOINT}`);
+      this.allBooks = books.data
+      console.log(this.allBooks)
+    },
+    countByGenre(idGenre){
+      let nbBookPerGenre= [];
+      nbBookPerGenre = this.allBooks.filter((book)=>{
+       book.genre.id = idGenre
+      })
+      console.log(nbBookPerGenre)
+      return "1"
+
+    }
+    
   },
     created(){
-      this.fetchAllGenres()
+      this.fetchAllGenres(),
+      this.fetchAllBooks(),
+      this.countByGenre()
+  },
+  computed:{
+    
   }
 }
 
